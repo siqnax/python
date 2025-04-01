42910ea2091617804121c2a7bbd655642d6dc0bc
siqnax <siqnax@gmail.com>
2025-04-01 21:55:06 +0300

    file update

---
+++ openf.py
@@ -0,0 +1,16 @@
+import os
+
+
+# READ FILE
+# file = open("func.py", "r")
+# print(file.read())
+
+
+# CREATE A NEW FILE
+# new_file = open("file.txt", "w")
+# new_file.write("This is a new file with some documents")
+
+# REMOVE A FILE 
+# os.remove("file.txt")
+
+
